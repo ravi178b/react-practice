6fd26bd92768b305cb6ca5f26fa3ab5748e5a2a9
ravi178b <ravi.bairagi@preciousinfosystem.com>
2023-05-04 12:54:08 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Profile from "./components/Profile";
+import Setting from "./components/Setting";
+import Dashboard from "./components/Dashboard"
+import SignUp from "./components/SignUp"
+import Login from "./components/Login"
 function App() {
   return (
-    <div>
-      <h1>App component</h1>
-    </div>
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <Navbar></Navbar>
+          <Routes>
+            <Route path="/home" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/dashboard" element={<Dashboard />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+            <Route path="/setting" element={<Setting />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { NavDropdown } from "react-bootstrap";
+
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <Link className="navbar-brand" href="#">
+              Menu
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse justify-content-end"
+              id="navbarNav"
+            >
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link
+                    className="nav-link active"
+                    aria-current="page"
+                    to="/home"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link active"
+                    aria-current="page"
+                    to="/about"
+                  >
+                    About
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link active"
+                    aria-current="page"
+                    to="/dashboard"
+                  >
+                    Dashboard
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link active"
+                    aria-current="page"
+                    to="/signup"
+                  >
+                    SignUp
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link active"
+                    aria-current="page"
+                    to="/login"
+                  >
+                    Login
+                  </Link>
+                </li>
+                <NavDropdown title="Dropdown" id="navbarScrollingDropdown">
+                  <NavDropdown.Item to="/profile">Profile</NavDropdown.Item>
+                  <NavDropdown.Item to="/setting">Setting</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                </NavDropdown>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
